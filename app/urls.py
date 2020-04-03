468e4d994abb2fc966cc7c44bc3bf95a408a4e2c
Mc <phenom.home@gmail.com>
2020-04-04 01:30:09 +0200

    Configure swagger.json

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path, include
-from rest_framework import routers
-
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
 
 router = routers.DefaultRouter()
+
+schema_view = get_schema_view(
+   openapi.Info(
+       title='Foodie API',
+       default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('', include(router.urls)),
     path('users', include('users.urls')),
+    path('swagger.json', schema_view.without_ui(), name='schema-json'),
 ]
