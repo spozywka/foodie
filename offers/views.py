0f20f04a132470117e1fb7d746661ef6f5a8cc0a
Mc <phenom.home@gmail.com>
2020-04-04 19:03:41 +0200

    Fix ordering.

--- offers/views.py
+++ offers/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
 
 from .models import Offer
 from .serializers import OfferSerializer
@@ -8,5 +9,6 @@ from .serializers import OfferSerializer
 class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ['category', 'city']
+    ordering = ['id']
